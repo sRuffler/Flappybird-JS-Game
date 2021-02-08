9b0fd040f7e2eaaaf84d444370aa8bd8b7d3a001
Stefan Ruffler <stefan_ruffler@live.com>
2021-02-08 22:09:59 +0000

    Refactors objects by using classes rather than function constructors. Also, prevents multiple jumps on holding spacebar down

--- Flappybird.js
+++ Flappybird.js
@@ -1,184 +1,193 @@
 
+class Component {
+    constructor(x, y, width, height, color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+    draw() {
+        gameManager.ctx.fillStyle = this.color;
+        gameManager.ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    hasCollidedWithRect(rect) {
+        return !(
+            ((this.y + this.height) < (rect.y)) ||
+            (this.y > (rect.y + rect.height)) ||
+            ((this.x + this.width) < rect.x) ||
+            (this.x > (rect.x + rect.width))
+        );
+    }
+}
+
+class Player extends Component {
+    constructor(x, y, width, height, color) {
+        super(x, y, width, height, color);
+        this.gravity = 0.05;
+        this.gravitySpeed = 0;
+        this.speedX = 0;
+        this.speedY = 0;
+    }
+    update() {
+        this.#setGravitySpeed();
+        this.y += this.speedY + this.gravitySpeed;
+        this.x += this.speedX;
+
+        if (this.y < 0 || this.y > gameManager.height) {
+            gameManager.stopGame();
+        }
+    }
+    #setGravitySpeed() {
+        this.gravitySpeed += this.gravity;
+
+        if (this.gravity < 0)
+            accelerate(0.15);
+
+        if (this.gravitySpeed >= 5)
+            this.gravitySpeed = 5;
+        else if (this.gravitySpeed <= -5)
+            this.gravitySpeed = -5;
+    }
+}
+
+class Column extends Component {
+
+    constructor(x, y, width, height, color, speedX) {
+        super(x, y, width, height, color);
+        this.speedX = speedX;
+    }
+    update() {
+        this.x += this.speedX;
+
+        if (this.hasCollidedWithRect(player))
+            gameManager.stopGame();
+    }
+    respawn(height, y) {
+        this.x += gameManager.width + 30;
+        this.height = height;
+        this.y = y;
+        gameManager.scoreFlag = false;
+    }
+
+}
 
 var player;
 var columns = [];
 
 var gameManager = {
 
-	canvas: document.getElementById('ctx'),
-	score:0,
-	scoreFlag:false,
-	stopped:true,
-	startGame: function(){
-		this.ctx = this.canvas.getContext('2d'); 
-		this.width = this.canvas.width;
-		this.height = this.canvas.height;
-		this.interval = setInterval(update, 1000 / 60);
-		this.score = 0;
-		this.stopped = false;
-	},
-	stopGame:function(){
-		clearInterval(this.interval);
-		columns = [];
-		player = null;
-		this.drawFinalScore();
-		this.stopped = true;
-	},
-	clear:function(){
-		this.ctx.clearRect(0,0, this.width, this.height);
-	},
-	drawScore:function(){
-		this.ctx.font = "30px Arial";
-		this.ctx.fillText(this.score, 430, 50);
-	},
-	drawFinalScore:function(){
-		this.clear();
-		this.ctx.font = "100px Arial";
-		if (this.score < 10)
-			this.ctx.fillText(this.score, 230, 270);
-		else if (this.score > 9 && this.score < 100)
-			this.ctx.fillText(this.score, 210, 270);
-		else
-			this.ctx.fillText(this.score, 190, 270);
-	}
+    canvas: document.getElementById('ctx'),
+    score: 0,
+    scoreFlag: false,
+    stopped: true,
+    startGame: function () {
+        this.ctx = this.canvas.getContext('2d');
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+        this.interval = setInterval(update, 1000 / 60);
+        this.score = 0;
+        this.stopped = false;
+    },
+    stopGame: function () {
+        clearInterval(this.interval);
+        columns = [];
+        player = null;
+        this.drawFinalScore();
+        this.stopped = true;
+    },
+    clear: function () {
+        this.ctx.clearRect(0, 0, this.width, this.height);
+    },
+    drawScore: function () {
+        this.ctx.font = "30px Arial";
+        this.ctx.fillText(this.score, 430, 50);
+    },
+    drawFinalScore: function () {
+        this.clear();
+        this.ctx.font = "100px Arial";
+        if (this.score < 10)
+            this.ctx.fillText(this.score, 230, 270);
+        else if (this.score > 9 && this.score < 100)
+            this.ctx.fillText(this.score, 210, 270);
+        else
+            this.ctx.fillText(this.score, 190, 270);
+    },
+    updateScore: function (player, column) {
+        if (player.x > column.x + column.width && !this.scoreFlag) {
+            this.scoreFlag = true;
+            this.score++;
+        }
+    }
 }
 
-function Column(x,y,width,height, speedX){
-	this.x = x;
-	this.y = y;
-	this.width = width;
-	this.height = height;
-	this.color = 'red';
-	this.speedX = speedX;
-	this.update = function(colHeight){
-		this.x += this.speedX;
-
-		if (isCollide(this,player))
-			gameManager.stopGame();		
-	}
-	this.respawn = function(height, y){
-		this.x += 530;
-		this.height = height;
-		this.y = y;
-		gameManager.scoreFlag = false;
-	}
-	this.draw = function(){
-		gameManager.ctx.fillStyle = this.color;
-		gameManager.ctx.fillRect(this.x,this.y,this.width,this.height);
-	}
+function startGame() {
+    gameManager.startGame();
+    player = new Player(30, 240, 20, 20, 'white');
+    columns.push(new Column(500, 0, 20, 200,'red', -5));
+    columns.push(new Column(500, 300, 20, 250,'red', -5));
 }
 
-function Player(x,y,width,height,color){
-	this.x = x;
-	this.y = y;
-	this.width = width;
-	this.height = height;
-	this.color = color;
-	this.gravity = 0.05;
-	this.gravitySpeed = 0;
-	this.speedX = 0;
-	this.speedY = 0;
-	this.update = function(){
-		this.gravitySpeed += this.gravity;
+function update() {
+    gameManager.clear();
 
-		// Limit gravity speed
-		if (this.gravitySpeed >= 5)
-			this.gravitySpeed = 5;
-		else if (this.gravitySpeed <= -10)
-			this.gravitySpeed = -10;
+    player.update();
 
-		this.y += this.speedY + this.gravitySpeed;
-		this.x += this.speedX;
+    gameManager.updateScore(player, columns[0]);
 
-		if (this.x > columns[0].x + columns[0].width && !gameManager.scoreFlag)
-		{
-			gameManager.scoreFlag = true;
-			gameManager.score++;
-		}
+    if (gameManager.stopped)
+        return;
 
-		if (this.y < 0 || this.y > gameManager.height)
-		{
-			gameManager.stopGame();
-		}
-	}
-	this.draw = function(){
-		gameManager.ctx.fillStyle = this.color;
-		gameManager.ctx.fillRect(this.x,this.y,this.width,this.height);
-	}
-}
+    var gapPos = Math.random() * 300 + 100;
+    var col1Height = gapPos - 50;
+    var col2Height = 500;
 
-function startGame(){
-	gameManager.startGame();
-	player = new Player(30,240,20,20,'white');
-	columns.push(new Column(500,0,20,200, -5));
-	columns.push(new Column(500,300,20,250,-5));
-}
+    var colY = [];
+    var colHeight = [];
 
-function update(){
-	gameManager.clear();
-	player.draw();
-	player.update();
+    colHeight.push(col1Height);
+    colHeight.push(col2Height);
 
-	if (gameManager.stopped)
-		return;
+    colY.push(0);
+    colY.push(gapPos + 50);
 
-	var gapPos = Math.random() * 300 + 100;
-	var col1Height = gapPos - 50;
-	var col2Height = 500;
+    for (var i = 0; i < columns.length; i++) {
 
-	var colY = [];
-	var colHeight = [];
+        columns[i].draw();
+        columns[i].update();
 
-	colHeight.push(col1Height);
-	colHeight.push(col2Height);
+        if (gameManager.stopped)
+            return;
 
-	colY.push(0);
-	colY.push(gapPos + 50);
+        if (columns[i].x < -20) {
+            columns[i].respawn(colHeight[i], colY[i]);
+        }
+    }
 
-	for(var i = 0; i < columns.length;i++){
-
-		columns[i].draw();
-		columns[i].update(colHeight[i]);
-
-		if (gameManager.stopped)
-		return;	
-
-		if (columns[i].x < -20)
-		{
-			columns[i].respawn(colHeight[i], colY[i]);			
-		}
-	}
-	
-	gameManager.drawScore(); 
+    player.draw();
+    gameManager.drawScore();
 }
 
 document.addEventListener('keydown', event => {
+    event.preventDefault();
+    if (event.repeat) {
+        accelerate(0.15);
+        return;
+    }
 
-  if (event.repeat) 
-  	return;
-
-  if (event.code === 'Space' && !gameManager.stopped) {
-    accelerate(-0.3);
-  }
+    if (event.code === 'Space' && !gameManager.stopped) {
+        accelerate(-5);
+    }
 });
 document.addEventListener('keyup', event => {
-  if (event.code === 'Space') {
-  	if (gameManager.stopped)
-  		startGame();
-  	else
-  		accelerate(0.1);
-  }
+    if (event.code === 'Space') {
+        if (gameManager.stopped)
+            startGame();
+        else
+            accelerate(0.15);
+    }
 });
 
 function accelerate(n) {
-  player.gravity = n;
-}
-
-function isCollide(a, b) {
-    return !(
-        ((a.y + a.height) < (b.y)) ||
-        (a.y > (b.y + b.height)) ||
-        ((a.x + a.width) < b.x) ||
-        (a.x > (b.x + b.width))
-    );
+    player.gravity = n;
 }
